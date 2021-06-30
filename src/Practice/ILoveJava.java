52180a3403ef2f1d90671587899f27291819ef21
TetianaKucherova <86332173+TetianaKucherova@users.noreply.github.com>
2021-06-30 01:13:43 -0400

    Update ILoveJava.java
    did changes on git

--- src/Practice/ILoveJava.java
+++ src/Practice/ILoveJava.java
@@ -7,6 +7,8 @@ public class ILoveJava {
 
         System.out.println ( "i love java" );
         System.out.println ( "i love java" );
+         System.out.println ( "i love java" ); 
+        System.out.println ( "i love java" );
     }
 }
 
