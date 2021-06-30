f1fcfe81f90beaed33d29bea05c3cc6d75c94d3d
Tetiana Kucherova <tkucherova1@gmail.com>
2021-06-30 00:56:12 -0400

    more additing

--- src/Practice/WorkFlow.java
+++ src/Practice/WorkFlow.java
@@ -1,4 +1,13 @@
 package Practice;
 
 public class WorkFlow {
+    public static void main ( String[] args ) {
+
+
+System.out.println("i love java");
+System.out.println("i love java");
+System.out.println("i love java");
+System.out.println("i love java");
+
+}
 }
