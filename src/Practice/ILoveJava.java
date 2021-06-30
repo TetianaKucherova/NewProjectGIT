7036e397153d754ce468c4286e1db755e026ed60
Tetiana Kucherova <tkucherova1@gmail.com>
2021-06-30 00:36:41 -0400

    added         System.out.println ( "i love java" );

---
+++ src/Practice/ILoveJava.java
@@ -0,0 +1,12 @@
+package Practice;
+
+public class ILoveJava {
+
+    public static void main ( String[] args ) {
+
+
+        System.out.println ( "i love java" );
+        System.out.println ( "i love java" );
+    }
+}
+
